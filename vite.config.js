9b33e95dc125d70966c9da45ff38d9219ee663dd
Loïc Chazeau <loic.chazeau@gmail.com>
2024-06-11 13:43:38 +0200

    change format in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
+        format: 'iife',
+        name: 'MyApp',
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
